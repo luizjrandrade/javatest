4dbe4bca5f92aa93204459989b533b51f79739b3
Luiz Junior <jr.luizandrade@gmail.com>
2021-05-07 10:09:54 -0300

    Envio class

---
+++ src/SomaTeste.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SomaTeste {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        double sum = a + b;
+
+        System.out.println(sum);
+
+
+    }
+}
